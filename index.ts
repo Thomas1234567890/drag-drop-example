1a10dd622d05a0890e5b864b01add5c3eaea6443
Thomas1234567890 <tompatcon@gmail.com>
2020-12-21 12:37:33 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+// Import stylesheets
+import "./style.css";
+console.clear();
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', (e) => {
+    draggable.classList.add('dragging')
+    console.log(`id : ${e.target.id} text : ${e.target.innerHTML}`)
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+const xxx = document.getElementById('1');
+xxx.addEventListener('click', () => {console.log('clicked')})
